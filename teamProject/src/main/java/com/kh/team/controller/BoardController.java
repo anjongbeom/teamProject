7cc55614c9892e2fc1d7b553a62a400fed632a9b
kerk0214 <kerk0214@gmail.com>
2022-06-07 12:09:05 +0900

    내부 페이지 작업

--- teamProject/src/main/java/com/kh/team/controller/BoardController.java
+++ teamProject/src/main/java/com/kh/team/controller/BoardController.java
@@ -1,12 +1,23 @@
 package com.kh.team.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.kh.team.vo.BoardVo;
+
+import service.BoardService;
+
+
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
 	
 	
 	@RequestMapping(value= "/menu1", method = RequestMethod.GET)
@@ -21,6 +32,22 @@ public class BoardController {
 	}
 	
 	
+	// ±Û µî·Ï
+	@RequestMapping(value = "/createRun", method = RequestMethod.POST)
+	public String create(BoardVo boardVo, RedirectAttributes rttr) {
+		System.out.println("BoardController, createRun, boardVo: " + boardVo);
+		boolean result = boardService.create(boardVo);
+		System.out.println("BoardController, createRun, result: "+ result);
+		rttr.addFlashAttribute("create_result", result);
+		return "redirect:/board/list";
+	}
+		
+	
+	
+	@RequestMapping(value= "/list", method = RequestMethod.GET)
+	public String list() {
+		return "/board/list";
+	}
 	
 
 }
---
+++ teamProject/src/main/java/service/BoardService.java
@@ -0,0 +1,19 @@
+package service;
+
+import java.util.List;
+
+import com.kh.team.vo.BoardVo;
+import com.kh.team.vo.PagingDto;
+
+
+public interface BoardService {
+	
+	public boolean create(BoardVo boardVo); // 글 쓰기 (Create)
+	public BoardVo read(int bno); // 글 읽기 (Read)
+	public boolean update(BoardVo boardVo); // 글 수정 (Update)
+	public boolean delete(int bno); // 글 삭제 (Delete)
+	public List<BoardVo> list(PagingDto pagingDto); // 글 목록 (Read)
+	public int getCount(PagingDto pagingDto);
+	public boolean insertReply(BoardVo boardVo); // 답글 작성
+	
+}
