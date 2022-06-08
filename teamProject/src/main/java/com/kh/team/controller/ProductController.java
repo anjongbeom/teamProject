4eb5fad33c39a5b429c0a46e2e500899688c4e64
kerk0214 <kerk0214@gmail.com>
2022-06-08 16:01:47 +0900

    menu1 forEach

--- teamProject/src/main/java/com/kh/team/controller/ProductController.java
+++ teamProject/src/main/java/com/kh/team/controller/ProductController.java
@@ -1,14 +1,17 @@
 package com.kh.team.controller;
 
+import java.io.FileInputStream;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kh.team.service.ProductService;
 import com.kh.team.vo.PagingDto;
@@ -45,6 +48,16 @@ public class ProductController {
 	}
 
 	
+	// 파일 이름으로 서버에서 이미지 가져오기
+	@RequestMapping(value = "/displayImage", method = RequestMethod.GET)
+	@ResponseBody
+	public byte[] displayImage(String filename) throws Exception {
+		FileInputStream fis;
+		fis = new FileInputStream(filename);
+		byte[] data = IOUtils.toByteArray(fis);
+		fis.close();
+		return data;
+	}
 	
 	
 
